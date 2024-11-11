e77341fc42bb293486dd0ae00245ddfb97a16a88
phucle103198 <52406323+phucle103198@users.noreply.github.com>
2024-11-11 09:26:16 +0700

    Update Cls.py

--- Classification/Cls.py
+++ Classification/Cls.py
@@ -1,14 +1,3 @@
-"""
-    -*- coding: utf-8 -*-
-    @Time   :2022/04/12 17:10
-    @Author : Pengyou FU
-    @blogs  : https://blog.csdn.net/Echo_Code?spm=1000.2115.3001.5343
-    @github : https://github.com/FuSiry/OpenSA
-    @WeChat : Fu_siry
-    @License：Apache-2.0 license
-
-"""
-
 from Classification.ClassicCls import ANN, SVM, PLS_DA, RF
 from Classification.CNN import CNN
 from Classification.SAE import SAE
