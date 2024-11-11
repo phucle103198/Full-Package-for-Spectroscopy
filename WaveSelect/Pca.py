8fbf382879bdc5cf5a6f3aea40d038680adb4688
phucle103198 <52406323+phucle103198@users.noreply.github.com>
2024-11-11 09:50:11 +0700

    Update Pca.py

--- WaveSelect/Pca.py
+++ WaveSelect/Pca.py
@@ -1,14 +1,3 @@
-"""
-    -*- coding: utf-8 -*-
-    @Time   :2022/04/12 17:10
-    @Author : Pengyou FU
-    @blogs  : https://blog.csdn.net/Echo_Code?spm=1000.2115.3001.5343
-    @github : https://github.com/FuSiry/OpenSA
-    @WeChat : Fu_siry
-    @License：Apache-2.0 license
-
-"""
-
 from sklearn.decomposition import PCA
 
 def Pca(X, nums=20):
@@ -17,7 +6,7 @@ def Pca(X, nums=20):
        :param nums: Number of principal components retained
        :return: X_reduction：Spectral data after dimensionality reduction
     """
-    pca = PCA(n_components=nums)  # 保留的特征数码
+    pca = PCA(n_components=nums) 
     pca.fit(X)
     X_reduction = pca.transform(X)
 
