96819fc1199ba4097fff5cfb12cfe7f8f6c2b06e
phucle103198 <52406323+phucle103198@users.noreply.github.com>
2024-11-11 09:36:58 +0700

    Update ClassicRgs.py

--- Regression/ClassicRgs.py
+++ Regression/ClassicRgs.py
@@ -1,18 +1,5 @@
-
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.neural_network import MLPRegressor
-# import hpelm
-
-"""
-    -*- coding: utf-8 -*-
-    @Time   :2022/04/12 17:10
-    @Author : Pengyou FU
-    @blogs  : https://blog.csdn.net/Echo_Code?spm=1000.2115.3001.5343
-    @github : https://github.com/FuSiry/OpenSA
-    @WeChat : Fu_siry
-    @License：Apache-2.0 license
-
-"""
 
 from sklearn.svm import SVR
 from Evaluate.RgsEvaluate import ModelRgsevaluate
