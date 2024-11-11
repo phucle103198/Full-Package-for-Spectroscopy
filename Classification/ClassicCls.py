d1c2066d3c4c2d09ee1621c84db0ba444cba4434
phucle103198 <52406323+phucle103198@users.noreply.github.com>
2024-11-11 09:26:00 +0700

    Update ClassicCls.py

--- Classification/ClassicCls.py
+++ Classification/ClassicCls.py
@@ -1,15 +1,3 @@
-"""
-    -*- coding: utf-8 -*-
-    @Time   :2022/04/12 17:10
-    @Author : Pengyou FU
-    @blogs  : https://blog.csdn.net/Echo_Code?spm=1000.2115.3001.5343
-    @github : https://github.com/FuSiry/OpenSA
-    @WeChat : Fu_siry
-    @License：Apache-2.0 license
-
-"""
-
-
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
@@ -22,14 +10,10 @@ import pandas  as pd
 def ANN(X_train, X_test, y_train, y_test, StandScaler=None):
 
     if StandScaler:
-        scaler = StandardScaler() # 标准化转换
+        scaler = StandardScaler()
         X_train = scaler.fit_transform(X_train)
         X_test = scaler.transform(X_test)
 
-    # 神经网络输入为2，第一隐藏层神经元个数为5，第二隐藏层神经元个数为2，输出结果为2分类。
-    # solver='lbfgs',  MLP的求解方法：L-BFGS 在小数据上表现较好，Adam 较为鲁棒，
-    # SGD在参数调整较优时会有最佳表现（分类效果与迭代次数）,SGD标识随机梯度下降。
-    #clf =  MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(8,8), random_state=1, activation='relu')
     clf =  MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto', beta_1=0.9,
                   beta_2=0.999, early_stopping=False, epsilon=1e-08,
                   hidden_layer_sizes=(10, 8), learning_rate='constant',
@@ -57,12 +41,9 @@ def SVM(X_train, X_test, y_train, y_test):
 def PLS_DA(X_train, X_test, y_train, y_test):
 
     y_train = pd.get_dummies(y_train)
-    # 建模
     model = PLSRegression(n_components=228)
     model.fit(X_train, y_train)
-    # 预测
     y_pred = model.predict(X_test)
-    # 将预测结果（类别矩阵）转换为数值标签
     y_pred = np.array([np.argmax(i) for i in y_pred])
     acc = accuracy_score(y_test, y_pred)
 
